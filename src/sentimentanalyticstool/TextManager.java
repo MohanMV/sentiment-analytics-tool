bf2646a9680e843d96d1ccb4e0aa936cee07a02c
Mohan Maha V <65824036+MohanMV@users.noreply.github.com>
2021-03-16 01:06:42 +0000

    Added a method that removes stop words
    Input text can be sorted and have its stop words removed. Also added a getter method for word list without stop words.

--- src/sentimentanalyticstool/TextManager.java
+++ src/sentimentanalyticstool/TextManager.java
@@ -5,42 +5,89 @@
  */
 package sentimentanalyticstool;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
+ * sorts text into array, removes stop words
  * @author Mohankumaar MV student-id = 17048038;
  */
 public class TextManager {
     
     String text;
-    String[] wordsList;
+    String[] wordListWithStopWords;
+    ArrayList<String> wordList = new ArrayList<>();
+    List<String> stopWords = new ArrayList<>();
+    ArrayList<String> wordListWithoutStopWords = new ArrayList<>();
             
-    public TextManager(String input){
+    public TextManager(String input) throws IOException{
         if(input == null || input.isEmpty()){
             throw new IllegalArgumentException("No text was provided.");
         }
         text = input;
-    }
-    
-    private void sortText(){
-        //regex removes numbers and punctuation marks 
-        String result = text.replaceAll("[\\d+|\\p{P}\\p{S}]" ,"");
-        wordsList = result.toLowerCase().split(" ");//split words based on spaces and changes to lowercase for afinn library
-        if(wordsList == null | wordsList.length == 0){
-            throw new IllegalArgumentException("Text is not descriptive enough");
+        try {
+            loadStopWords();
+        } catch (IOException ex) {
+           Logger.getLogger(TextManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    public void sortText(){
+        //regex removes numbers and punctuation marks 
+        String result = text.replaceAll("[\\d+|\\p{P}\\p{S}]" ,"");
+        wordListWithStopWords = result.toLowerCase().split(" ");//split words based on spaces and changes to lowercase for afinn library
 
+        if(wordListWithStopWords == null | wordListWithStopWords.length == 0){
+            throw new IllegalArgumentException("No proper text to analyse");// need to work more on this
+        } else{
+            wordList.addAll(Arrays.asList(wordListWithStopWords));
+            wordListWithoutStopWords.addAll(Arrays.asList(wordListWithStopWords));
+        }
+    }
+       
+    public void removeStopWords(){
+
+        for(String word : wordList){
+            for(String sWord : stopWords){
+                if(word.equals(sWord)){
+                    wordListWithoutStopWords.remove(sWord);
+                }
+            }
+        }
+    }
+    
+    public ArrayList<String> getWordList(){
+        return wordListWithoutStopWords;
+    }
+    
+    public void loadStopWords() throws FileNotFoundException, IOException{
+        
+        BufferedReader buffreader = new BufferedReader(new FileReader("src/stopwords/scikitlearn.txt"));
+        String sWord;
+        while ((sWord = buffreader.readLine()) != null) 
+        {
+            stopWords.add(sWord);
+        }
+        buffreader.close();
+    }
+        
+    
+    
     @Override
     public String toString(){
-        sortText();
+        
         String output = "";
-        for(String word : wordsList){
+        for(String word : wordListWithoutStopWords){
             output += word + "\n";
         }
         return output;
--- src/sentimentanalyticstool/UI.java
+++ src/sentimentanalyticstool/UI.java
@@ -5,6 +5,7 @@
  */
 package sentimentanalyticstool;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +18,19 @@ public class UI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         
         TextManager input = new TextManager(" y£)O ___00000*7&%*^&$%*&^MA22222MA A BI108*&^5G a$$SS ^B1111iT5875765$*4)(*&()*67c&^^h 68897F9089u8c987&^^%k ");
+        input.sortText();
+        input.removeStopWords();
+        ArrayList<String> words = new ArrayList<>();
+        words = input.getWordList();
         System.out.println(input.toString());
+        /// Choose language:
+        // Enter text
+        // out sentiment 
+ 
         
     }
     
